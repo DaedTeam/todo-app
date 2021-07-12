9b84c1c7f7e7d4ef00785fc393f7af2e5c3b703e
TikTzuki <tranphanthanhlong18@gmail.com>
2021-07-12 22:04:54 +0700

    [add]: test connect mongodb

--- app/application.py
+++ app/application.py
@@ -1,10 +1,15 @@
+import os
+
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import MongoClient
 
 from app.api.v1.routers import routers, note_router, label_router, user_router, issue_router, task_router
 from app.containers import Container
 
 
 def create_app():
+    test_mongodb()
     container = Container()
     container.config.from_yaml('config.yml')
     container.wire(modules=[routers, user_router, issue_router, note_router, label_router, task_router])
@@ -18,6 +23,18 @@ def create_app():
     app.container = container
     app.include_router(routers.router)
     return app
+
+
+def test_mongodb():
+    url = os.getenv("MONGODB", "mongodb+srv://tik:123@cluster0.bh9kt.mongodb.net/cube_engine?retryWrites=true&w=majority")
+    db_name = os.getenv("MONGODB", "cube_engine")
+    client = MongoClient(url)
+    db = client[db_name]
+    collection_name = "users"
+    collection = db[collection_name]
+    d = collection.find_one()
+    print('\t ------- Test mongo db ----------')
+    print(d)
 
 
 if __name__ == '__main__':
